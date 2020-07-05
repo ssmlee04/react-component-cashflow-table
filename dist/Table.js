e13bf479fe0bef57ee8d1487792367d64d45a808
Shih-Min Lee <ssmlee04@gmail.com>
2020-07-05 18:35:55 -0400

    initial commit

---
+++ dist/Table.js
@@ -0,0 +1,143 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = exports.Table = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _lodash = _interopRequireDefault(require("lodash"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Table = /*#__PURE__*/function (_React$Component) {
+  _inherits(Table, _React$Component);
+
+  var _super = _createSuper(Table);
+
+  function Table() {
+    _classCallCheck(this, Table);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(Table, [{
+    key: "render",
+    value: function render() {
+      var data = this.props.data;
+
+      if (!data || !data.length) {
+        return /*#__PURE__*/_react["default"].createElement("div", {
+          className: "font-12"
+        }, "Not available at this time... ");
+      }
+
+      var unit = _lodash["default"].get(data, '0.unit') || 'million';
+      var arr = data.slice(-4);
+      return /*#__PURE__*/_react["default"].createElement("table", {
+        className: "table table-sm"
+      }, /*#__PURE__*/_react["default"].createElement("thead", {
+        className: "bold"
+      }, /*#__PURE__*/_react["default"].createElement("th", {
+        className: "left lighter"
+      }, "Unit: (", unit, ")"), /*#__PURE__*/_react["default"].createElement("th", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[0] && arr[0].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[1] && arr[1].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[2] && arr[2].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[3] && arr[3].quarterStr)), /*#__PURE__*/_react["default"].createElement("tbody", null, /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Total Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[3] && arr[3].cashFlow && parseFloat(arr[3].cashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[2] && arr[1].cashFlow && parseFloat(arr[2].cashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[1] && arr[2].cashFlow && parseFloat(arr[1].cashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[0] && arr[3].cashFlow && parseFloat(arr[0].cashFlow).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Financing Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[3] && arr[3].cashFlowFinancing && parseFloat(arr[3].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[2] && arr[1].cashFlowFinancing && parseFloat(arr[2].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[1] && arr[2].cashFlowFinancing && parseFloat(arr[1].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[0] && arr[3].cashFlowFinancing && parseFloat(arr[0].cashFlowFinancing).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Investing Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[0] && arr[0].totalInvestingCashFlows && parseFloat(arr[0].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[1] && arr[1].totalInvestingCashFlows && parseFloat(arr[1].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[2] && arr[2].totalInvestingCashFlows && parseFloat(arr[2].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[3] && arr[3].totalInvestingCashFlows && parseFloat(arr[3].totalInvestingCashFlows).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Operating Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[0] && arr[0].operatingCashFlow && parseFloat(arr[0].operatingCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[1] && arr[1].operatingCashFlow && parseFloat(arr[1].operatingCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[2] && arr[2].operatingCashFlow && parseFloat(arr[2].operatingCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[3] && arr[3].operatingCashFlow && parseFloat(arr[3].operatingCashFlow).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Total Investments"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[0] && arr[0].investments && parseFloat(arr[0].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[1] && arr[1].investments && parseFloat(arr[1].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[2] && arr[2].investments && parseFloat(arr[2].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[3] && arr[3].investments && parseFloat(arr[3].investments).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bold"
+      }, "Total Capital Expendistures"), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-5"
+      }, arr[0] && arr[0].capitalExpenditures && parseFloat(arr[0].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-4"
+      }, arr[1] && arr[1].capitalExpenditures && parseFloat(arr[1].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-3"
+      }, arr[2] && arr[2].capitalExpenditures && parseFloat(arr[2].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
+        className: "bg-lightgray-ultra-2"
+      }, arr[3] && arr[3].capitalExpenditures && parseFloat(arr[3].capitalExpenditures).toFixed(2)))));
+    }
+  }]);
+
+  return Table;
+}(_react["default"].Component);
+
+exports.Table = Table;
+var _default = Table;
+exports["default"] = _default;
---
+++ dist/index.js
@@ -0,0 +1,173 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = exports.CashflowTable = void 0;
+
+var _lodash = _interopRequireDefault(require("lodash"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Table = _interopRequireDefault(require("./Table"));
+
+var _reactCopyToClipboard = require("react-copy-to-clipboard");
+
+require("./../index.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var calculateCashflows = function calculateCashflows(data) {
+  var divider = 1000000;
+  var unit = 'million';
+  var u = 'm';
+  if (!data || !data.length) return data;
+
+  if (data[0].cf > 1000000000) {
+    divider = 1000000000;
+    unit = 'billion';
+    u = 'b';
+  }
+
+  data = _lodash["default"].sortBy(data.filter(function (d) {
+    return d.reportDate;
+  }), function (d) {
+    return -d.reportDate;
+  }).reverse();
+  return data.map(function (d, i) {
+    var qq = ~~d.reportDate.slice(5, 7);
+    var yy = d.reportDate.slice(0, 4);
+    var qtr;
+
+    if (qq <= 3) {
+      qtr = 'Q1';
+    } else if (qq <= 6) {
+      qtr = 'Q2';
+    } else if (qq <= 9) {
+      qtr = 'Q3';
+    } else if (qq <= 12) {
+      qtr = 'Q4';
+    }
+
+    d.quarterStr = yy + qtr;
+    d.unit = unit;
+    d.u = u;
+    d.cashFlow = d.cf / divider;
+    d.cashFlowFinancing = d.cff / divider;
+    d.totalInvestingCashFlows = d.tic / divider;
+    d.investments = d.iv / divider;
+    d.capitalExpenditures = d.ce / divider;
+    return d;
+  });
+};
+
+var CashflowTable = /*#__PURE__*/function (_React$Component) {
+  _inherits(CashflowTable, _React$Component);
+
+  var _super = _createSuper(CashflowTable);
+
+  function CashflowTable(props) {
+    var _this;
+
+    _classCallCheck(this, CashflowTable);
+
+    _this = _super.call(this, props);
+    _this.state = {};
+    return _this;
+  }
+
+  _createClass(CashflowTable, [{
+    key: "shouldComponentUpdate",
+    value: function shouldComponentUpdate(nextProps, nextState) {
+      var profile = this.props.profile;
+      if (!profile) return true;
+      if (nextState.copied) return true;
+      if (profile.ticker !== nextProps.profile.ticker) return true;
+      return false;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var profile = this.props.profile;
+      var copied = this.state.copied;
+
+      if (!profile) {
+        return /*#__PURE__*/_react["default"].createElement("div", {
+          style: {
+            fontSize: 14
+          }
+        }, "Not available at this time... ");
+      }
+
+      if (profile.cashflow_table && profile.cashflow_table.url) {
+        var btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-10' : 'react-components-show-url btn btn-sm btn-warning font-10';
+        var btnText = copied ? 'Copied' : 'Copy Img';
+        return /*#__PURE__*/_react["default"].createElement("div", {
+          className: "react-components-show-button"
+        }, /*#__PURE__*/_react["default"].createElement("img", {
+          alt: "".concat(profile.ticker, " - ").concat(profile.name, " revenue and income margins"),
+          src: profile.cashflow_table.url,
+          style: {
+            width: '100%'
+          }
+        }), /*#__PURE__*/_react["default"].createElement(_reactCopyToClipboard.CopyToClipboard, {
+          text: profile.cashflow_table.url || '',
+          onCopy: function onCopy() {
+            return _this2.setState({
+              copied: true
+            });
+          }
+        }, /*#__PURE__*/_react["default"].createElement("button", {
+          className: btnClass,
+          value: btnText
+        }, btnText)));
+      }
+
+      var data = calculateCashflows(_lodash["default"].get(profile, 'cashflow_stmt.data', []));
+      return /*#__PURE__*/_react["default"].createElement("div", {
+        style: {
+          width: '100%',
+          padding: 5,
+          fontSize: 14
+        }
+      }, /*#__PURE__*/_react["default"].createElement("div", {
+        style: {
+          color: 'darkred',
+          fontWeight: 'bold'
+        }
+      }, profile.ticker, " - ", profile.name), /*#__PURE__*/_react["default"].createElement(_Table["default"], {
+        data: data
+      }));
+    }
+  }]);
+
+  return CashflowTable;
+}(_react["default"].Component);
+
+exports.CashflowTable = CashflowTable;
+var _default = CashflowTable;
+exports["default"] = _default;
