ff99e6ebc78c6fa6e64042ced286473b15b644c4
Shih-Min Lee <ssmlee04@Shih-Mins-MacBook-Pro.local>
2021-05-26 00:53:24 -0400

    major changes to ui

--- dist/Table.js
+++ dist/Table.js
@@ -33,10 +33,10 @@ function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Re
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-var color = function color(num) {
-  if (num > 0) return 'green bold';
-  if (num < 0) return 'red bold';
-  return;
+var greenOrRed = function greenOrRed(str, high, low) {
+  var v = parseFloat(str);
+  if (v > high) return 'green bold';
+  if (v < low) return 'red bold';
 };
 
 var Table = /*#__PURE__*/function (_React$Component) {
@@ -53,11 +53,14 @@ var Table = /*#__PURE__*/function (_React$Component) {
   _createClass(Table, [{
     key: "render",
     value: function render() {
-      var data = this.props.data;
+      var _this$props = this.props,
+          data = _this$props.data,
+          _this$props$count = _this$props.count,
+          count = _this$props$count === void 0 ? 4 : _this$props$count;
 
       if (!data || !data.length) {
         return /*#__PURE__*/_react["default"].createElement("div", {
-          className: "font-8"
+          className: "font-12"
         }, "Not available at this time... ");
       }
 
@@ -66,91 +69,60 @@ var Table = /*#__PURE__*/function (_React$Component) {
         className: "table table-sm",
         style: {
           marginBottom: 0,
-          fontSize: 7
+          fontSize: 10
         }
       }, /*#__PURE__*/_react["default"].createElement("thead", {
         className: "bold"
       }, /*#__PURE__*/_react["default"].createElement("th", {
         className: "left lighter"
-      }, "Unit: (", unit, ")"), /*#__PURE__*/_react["default"].createElement("th", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].quarterStr), /*#__PURE__*/_react["default"].createElement("th", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].quarterStr)), /*#__PURE__*/_react["default"].createElement("tbody", null, /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Unit: (", unit, ")"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d)
+        }, data[d] && data[d].quarterStr);
+      })), /*#__PURE__*/_react["default"].createElement("tbody", null, /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Operating Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].cashFlowOperating && parseFloat(data[0].cashFlowOperating).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].cashFlowOperating && parseFloat(data[1].cashFlowOperating).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].cashFlowOperating && parseFloat(data[2].cashFlowOperating).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].cashFlowOperating && parseFloat(data[3].cashFlowOperating).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Operating Cashflows"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter ").concat(greenOrRed(data[d] && data[d].cashFlowOperating, 0, -999999999999999))
+        }, data[d] && data[d].cashFlowOperating && parseFloat(data[d].cashFlowOperating).toFixed(2));
+      })), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Financing Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].cashFlowFinancing && parseFloat(data[0].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].cashFlowFinancing && parseFloat(data[1].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].cashFlowFinancing && parseFloat(data[2].cashFlowFinancing).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].cashFlowFinancing && parseFloat(data[3].cashFlowFinancing).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Financing Cashflows"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter")
+        }, data[d] && data[d].cashFlowFinancing && parseFloat(data[d].cashFlowFinancing).toFixed(2));
+      })), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Investing Cashflows"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].totalInvestingCashFlows && parseFloat(data[0].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].totalInvestingCashFlows && parseFloat(data[1].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].totalInvestingCashFlows && parseFloat(data[2].totalInvestingCashFlows).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].totalInvestingCashFlows && parseFloat(data[3].totalInvestingCashFlows).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Investing Cashflows"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter")
+        }, data[d] && data[d].totalInvestingCashFlows && parseFloat(data[d].totalInvestingCashFlows).toFixed(2));
+      })), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Total Investments"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].investments && parseFloat(data[0].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].investments && parseFloat(data[1].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].investments && parseFloat(data[2].investments).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].investments && parseFloat(data[3].investments).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Total Capital Expenditures"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter")
+        }, data[d] && data[d].capitalExpenditures && parseFloat(data[d].capitalExpenditures).toFixed(2));
+      })), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Total Capital Expenditures"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5"
-      }, data[0] && data[0].capitalExpenditures && parseFloat(data[0].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4"
-      }, data[1] && data[1].capitalExpenditures && parseFloat(data[1].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3"
-      }, data[2] && data[2].capitalExpenditures && parseFloat(data[2].capitalExpenditures).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2"
-      }, data[3] && data[3].capitalExpenditures && parseFloat(data[3].capitalExpenditures).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
+      }, "Changes In Cash"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter ").concat(greenOrRed(data[d] && data[d].changeInCash, 0, -999999999999999))
+        }, data[d] && data[d].changeInCash && parseFloat(data[d].changeInCash).toFixed(2));
+      })), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
         className: "bold"
-      }, "Changes In Cash"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5 ".concat(color(data[0] && data[0].changeInCash))
-      }, data[0] && data[0].changeInCash && parseFloat(data[0].changeInCash).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4 ".concat(color(data[1] && data[1].changeInCash))
-      }, data[1] && data[1].changeInCash && parseFloat(data[1].changeInCash).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3 ".concat(color(data[2] && data[2].changeInCash))
-      }, data[2] && data[2].changeInCash && parseFloat(data[2].changeInCash).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2 ".concat(color(data[3] && data[3].changeInCash))
-      }, data[3] && data[3].changeInCash && parseFloat(data[3].changeInCash).toFixed(2))), /*#__PURE__*/_react["default"].createElement("tr", null, /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bold"
-      }, "Free Cash flow"), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-5 ".concat(color(data[0] && data[0].freeCashFlow))
-      }, data[0] && data[0].freeCashFlow && parseFloat(data[0].freeCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-4 ".concat(color(data[1] && data[1].freeCashFlow))
-      }, data[1] && data[1].freeCashFlow && parseFloat(data[1].freeCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-3 ".concat(color(data[2] && data[2].freeCashFlow))
-      }, data[2] && data[2].freeCashFlow && parseFloat(data[2].freeCashFlow).toFixed(2)), /*#__PURE__*/_react["default"].createElement("td", {
-        className: "bg-lightgray-ultra-2 ".concat(color(data[3] && data[3].freeCashFlow))
-      }, data[3] && data[3].freeCashFlow && parseFloat(data[3].freeCashFlow).toFixed(2)))));
+      }, "Free Cash flow"), _lodash["default"].range(count).map(function (d) {
+        return /*#__PURE__*/_react["default"].createElement("td", {
+          key: d,
+          className: "bg-lightgray-ul-".concat(d, " hov lighter ").concat(greenOrRed(data[d] && data[d].freeCashFlow, 0, -999999999999999))
+        }, data[d] && data[d].freeCashFlow && parseFloat(data[d].freeCashFlow).toFixed(2));
+      }))));
     }
   }]);
 
--- src/Table.js
+++ src/Table.js
@@ -1,80 +1,52 @@
 import React from 'react';
 import _ from 'lodash';
 
-const color = (num) => {
-  if (num > 0) return 'green bold';
-  if (num < 0) return 'red bold';
-  return;
+const greenOrRed = (str, high, low) => {
+  const v = parseFloat(str);
+  if (v > high) return 'green bold';
+  if (v < low) return 'red bold';
 };
 
 export class Table extends React.Component {
   render() {
-    const { data } = this.props;
+    const { data, count = 4 } = this.props;
     if (!data || !data.length) {
       return (
-        <div className='font-8'>Not available at this time... </div>
+        <div className='font-12'>Not available at this time... </div>
       );
     }
     const unit = _.get(data, '0.unit') || 'million';
 
     return (
-      <table className='table table-sm' style={{ marginBottom: 0, fontSize: 7 }}>
+      <table className='table table-sm' style={{ marginBottom: 0, fontSize: 10 }}>
         <thead className='bold'>
           <th className='left lighter'>Unit: ({unit})</th>
-          <th className='bg-lightgray-ultra-5'>{data[0] && data[0].quarterStr}</th>
-          <th className='bg-lightgray-ultra-4'>{data[1] && data[1].quarterStr}</th>
-          <th className='bg-lightgray-ultra-3'>{data[2] && data[2].quarterStr}</th>
-          <th className='bg-lightgray-ultra-2'>{data[3] && data[3].quarterStr}</th>
+          {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d}`}>{data[d] && data[d].quarterStr}</td>)}
         </thead>
         <tbody>
           <tr>
             <td className='bold'>Operating Cashflows</td>
-            <td className='bg-lightgray-ultra-5'>{data[0] && data[0].cashFlowOperating && parseFloat(data[0].cashFlowOperating).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-4'>{data[1] && data[1].cashFlowOperating && parseFloat(data[1].cashFlowOperating).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-3'>{data[2] && data[2].cashFlowOperating && parseFloat(data[2].cashFlowOperating).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-2'>{data[3] && data[3].cashFlowOperating && parseFloat(data[3].cashFlowOperating).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter ${greenOrRed(data[d] && data[d].cashFlowOperating, 0, -999999999999999)}`}>{data[d] && data[d].cashFlowOperating && parseFloat(data[d].cashFlowOperating).toFixed(2)}</td>)}
           </tr>
           <tr>
             <td className='bold'>Financing Cashflows</td>
-            <td className='bg-lightgray-ultra-5'>{data[0] && data[0].cashFlowFinancing && parseFloat(data[0].cashFlowFinancing).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-4'>{data[1] && data[1].cashFlowFinancing && parseFloat(data[1].cashFlowFinancing).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-3'>{data[2] && data[2].cashFlowFinancing && parseFloat(data[2].cashFlowFinancing).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-2'>{data[3] && data[3].cashFlowFinancing && parseFloat(data[3].cashFlowFinancing).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter`}>{data[d] && data[d].cashFlowFinancing && parseFloat(data[d].cashFlowFinancing).toFixed(2)}</td>)}
           </tr>
           <tr>
             <td className='bold'>Investing Cashflows</td>
-            <td className='bg-lightgray-ultra-5'>{data[0] && data[0].totalInvestingCashFlows && parseFloat(data[0].totalInvestingCashFlows).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-4'>{data[1] && data[1].totalInvestingCashFlows && parseFloat(data[1].totalInvestingCashFlows).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-3'>{data[2] && data[2].totalInvestingCashFlows && parseFloat(data[2].totalInvestingCashFlows).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-2'>{data[3] && data[3].totalInvestingCashFlows && parseFloat(data[3].totalInvestingCashFlows).toFixed(2)}</td>
-          </tr>
-          <tr>
-            <td className='bold'>Total Investments</td>
-            <td className='bg-lightgray-ultra-5'>{data[0] && data[0].investments && parseFloat(data[0].investments).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-4'>{data[1] && data[1].investments && parseFloat(data[1].investments).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-3'>{data[2] && data[2].investments && parseFloat(data[2].investments).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-2'>{data[3] && data[3].investments && parseFloat(data[3].investments).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter`}>{data[d] && data[d].totalInvestingCashFlows && parseFloat(data[d].totalInvestingCashFlows).toFixed(2)}</td>)}
           </tr>
           <tr>
             <td className='bold'>Total Capital Expenditures</td>
-            <td className='bg-lightgray-ultra-5'>{data[0] && data[0].capitalExpenditures && parseFloat(data[0].capitalExpenditures).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-4'>{data[1] && data[1].capitalExpenditures && parseFloat(data[1].capitalExpenditures).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-3'>{data[2] && data[2].capitalExpenditures && parseFloat(data[2].capitalExpenditures).toFixed(2)}</td>
-            <td className='bg-lightgray-ultra-2'>{data[3] && data[3].capitalExpenditures && parseFloat(data[3].capitalExpenditures).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter`}>{data[d] && data[d].capitalExpenditures && parseFloat(data[d].capitalExpenditures).toFixed(2)}</td>)}
           </tr>
           <tr>
             <td className='bold'>Changes In Cash</td>
-            <td className={`bg-lightgray-ultra-5 ${color(data[0] && data[0].changeInCash)}`}>{data[0] && data[0].changeInCash && parseFloat(data[0].changeInCash).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-4 ${color(data[1] && data[1].changeInCash)}`}>{data[1] && data[1].changeInCash && parseFloat(data[1].changeInCash).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-3 ${color(data[2] && data[2].changeInCash)}`}>{data[2] && data[2].changeInCash && parseFloat(data[2].changeInCash).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-2 ${color(data[3] && data[3].changeInCash)}`}>{data[3] && data[3].changeInCash && parseFloat(data[3].changeInCash).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter ${greenOrRed(data[d] && data[d].changeInCash, 0, -999999999999999)}`}>{data[d] && data[d].changeInCash && parseFloat(data[d].changeInCash).toFixed(2)}</td>)}
           </tr>
           <tr>
             <td className='bold'>Free Cash flow</td>
-            <td className={`bg-lightgray-ultra-5 ${color(data[0] && data[0].freeCashFlow)}`}>{data[0] && data[0].freeCashFlow && parseFloat(data[0].freeCashFlow).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-4 ${color(data[1] && data[1].freeCashFlow)}`}>{data[1] && data[1].freeCashFlow && parseFloat(data[1].freeCashFlow).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-3 ${color(data[2] && data[2].freeCashFlow)}`}>{data[2] && data[2].freeCashFlow && parseFloat(data[2].freeCashFlow).toFixed(2)}</td>
-            <td className={`bg-lightgray-ultra-2 ${color(data[3] && data[3].freeCashFlow)}`}>{data[3] && data[3].freeCashFlow && parseFloat(data[3].freeCashFlow).toFixed(2)}</td>
+            {_.range(count).map(d => <td key={d} className={`bg-lightgray-ul-${d} hov lighter ${greenOrRed(data[d] && data[d].freeCashFlow, 0, -999999999999999)}`}>{data[d] && data[d].freeCashFlow && parseFloat(data[d].freeCashFlow).toFixed(2)}</td>)}
           </tr>
         </tbody>
       </table>
