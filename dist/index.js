74d8fbcad65d17cdefec2cc43f4c866715cab841
Shih-Min Lee <ssmlee04@Shih-Mins-MacBook-Pro.local>
2021-05-30 18:06:29 -0400

    babel transform css

--- dist/index.js
+++ dist/index.js
@@ -13,8 +13,6 @@ var _Table = _interopRequireDefault(require("./Table"));
 
 var _reactCopyToClipboard = require("react-copy-to-clipboard");
 
-require("./../index.css");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
